a6bc8236069b4b588d8bc31c52996ef18b39a27c
Jay13238 <115621927+Jay13238@users.noreply.github.com>
2024-10-28 01:14:38 +0200

    about page

---
+++ storefront/src/app/[countryCode]/(main)/about/page.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default function page() {
+  return (
+    <div className="pt-24 max-w-lg w-full px-6 md:px-0 md:mx-auto">
+      <h1 className="text-center mb-10">About Us</h1>
+      <p className="mb-10 text-primary">
+        The Preparation Ministry is a self-supporting ministry dedicated to
+        partnering with its local conference to reach people through public
+        seminars and visual media. Over the past 11 years, God has abundantly
+        blessed our efforts, allowing us to minister to more than 70 churches.
+        Our sermons and health programs have been broadcast on five different
+        Christian TV channels, expanding our outreach to a broader audience.
+        <br />
+        <br />
+        Our mission focuses on helping people spiritually, mentally, and
+        physically. We conduct seminars covering a wide range of topics,
+        including spiritual growth, end-time events, emotional healing, health,
+        cooking classes, marriage, parenting, and men's ministries. One of our
+        most popular programs is the "Personality" series, which has received
+        widespread acclaim. The ministry is led by Renier and Chantell Horn,
+        along with their two sons, Jonathan and Matthew. With over a decade of
+        ministry experience, Renier and Chantell have worked with three major
+        conferences in South Africa.
+      </p>
+    </div>
+  )
+}
