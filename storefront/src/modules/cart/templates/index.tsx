5594cb56aaabd645b790914ddd44362022ee8294
Jay13238 <115621927+Jay13238@users.noreply.github.com>
2024-10-28 10:04:42 +0200

    Remove SignInPrompt and Divider components from CartTemplate when customer is not logged in.

--- storefront/src/modules/cart/templates/index.tsx
+++ storefront/src/modules/cart/templates/index.tsx
@@ -18,12 +18,7 @@ const CartTemplate = ({
         {cart?.items?.length ? (
           <div className="grid grid-cols-1 small:grid-cols-[1fr_360px] gap-x-40">
             <div className="flex flex-col bg-white py-6 gap-y-6">
-              {!customer && (
-                <>
-                  <SignInPrompt />
-                  <Divider />
-                </>
-              )}
+
               <ItemsTemplate items={cart?.items} />
             </div>
             <div className="relative">
