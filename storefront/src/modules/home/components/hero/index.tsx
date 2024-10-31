6ed9d025b4ce77e66a5e3bc9a678c590f317f632
Jay13238 <115621927+Jay13238@users.noreply.github.com>
2024-10-31 11:53:49 +0200

    image

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -9,8 +9,8 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 
 // List of images for the slider
 import heroImage1 from "../../../../../public/hero1.jpeg"
-import heroImage2 from "../../../../../public/hero2.jpeg"
-import heroImage3 from "../../../../../public/hero3.jpeg"
+import heroImage2 from "../../../../../public/div.jpg"
+import heroImage3 from "../../../../../public/hero.jpeg"
 
 const images = [heroImage1, heroImage2, heroImage3]
 
