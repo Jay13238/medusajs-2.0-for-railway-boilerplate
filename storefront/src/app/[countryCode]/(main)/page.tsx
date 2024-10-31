7da07e1cdfcdf2640100fd49097f103ad936855a
Jay13238 <115621927+Jay13238@users.noreply.github.com>
2024-10-31 14:20:45 +0200

    Remove FeaturedProducts component from Home page.

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -124,7 +124,7 @@ export default async function Home({ params: { countryCode } }) {
 
         {/* Featured Products */}
         <ul className="flex flex-col gap-x-6">
-          <FeaturedProducts collections={collections} region={region} />
+  
         </ul>
       </div>
     </>
